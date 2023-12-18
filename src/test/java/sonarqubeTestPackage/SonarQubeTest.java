a106a860abfc2e6b4a931dc7e6c9a318f2eb27dd
NitheshHS <nitheshahs@tekion.com>
2023-12-18 20:57:59 +0530

    new PR

---
+++ src/test/java/sonarqubeTestPackage/SonarQubeTest.java
@@ -0,0 +1,14 @@
+package sonarqubeTestPackage;
+
+public class SonarQubeTest {
+    static int var1$=10;
+
+    public static void main(String[] args) {
+        System.out.println(var1$);
+        System.out.println(var1$+1);
+        System.out.println(var1$+1);
+        System.out.println(var1$+1);
+        System.out.println(var1$+1);
+
+    }
+}
